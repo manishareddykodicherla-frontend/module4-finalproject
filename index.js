fcbaab7fe0db764fa242fe09e1e5c12cdb770282
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-03-31 10:39:28 -0700

    header completed

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ async function showTime(filter){
  const showHours= await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=9a6c4194&S=movie`);
  const showMinutes= await showHours.json();
  moviesData=showMinutes.Search;
- console.log(moviesData)
- let movies=document.querySelector(".movie__list");
- movies.innerHTML= showMinutes.Search.map((showHour)=>movieTime(showHour)).join("");
+ 
+ let movies =document.querySelector(".movie__list");
+ movies.innerHTML= showMinutes.Search.map((movie)=>movieTime(movie)).join("");
   if(filter==="movie Year"){
     console.log(filter)
- const filteredMovies=showMinutes.Search.sort((a,b)=>a.Year - b.Year);
- console.log(filteredMovies)
-    }
+ const filteredMovies=moviesData.sort((a,b)=>b.Year - a.Year);
+ let movies = document.querySelector(".movie__list");
+movies.innerHTML=filteredMovies.map(movie=>movieTime(movie)).join("")
+  }
+  if (filter === "movieName") {
+  const sortedMovies = moviesData.sort((a, b) =>
+    a.Title.localeCompare(b.Title)
+  );
+
+  let movies = document.querySelector(".movie__list");
+  movies.innerHTML = sortedMovies.map(movie => movieTime(movie)).join("");
+}
 }
 
 showTime()
@@ -31,7 +40,7 @@ showTime()
  }
  function movieSearch(event){
     console.log(event)
-    showTime(event.target.value)
+    showTime(event.target.value)//The value event.target.value is same as filter==="movieName" in if condition 
   
  }
  
