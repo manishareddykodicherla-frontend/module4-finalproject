6e93f802fd7b0ac7d5082a21ae6645c36f262995
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-03-31 11:52:14 -0700

    header completed

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 // API KEY: http://www.omdbapi.com/?i=tt3896198&apikey=9a6c4194
 let moviesData=[]
 async function showTime(filter){
- const showHours= await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=9a6c4194&S=movie`);
+ const showHours= await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=9a6c4194&s=movie`);
  const showMinutes= await showHours.json();
  moviesData=showMinutes.Search;
  
- let movies =document.querySelector(".movie__list");
+ let movies =document.querySelector(".movie__lists");
  movies.innerHTML= showMinutes.Search.map((movie)=>movieTime(movie)).join("");
   if(filter==="movie Year"){
     console.log(filter)
  const filteredMovies=moviesData.sort((a,b)=>b.Year - a.Year);
- let movies = document.querySelector(".movie__list");
+ let movies = document.querySelector(".movie__lists");
 movies.innerHTML=filteredMovies.map(movie=>movieTime(movie)).join("")
   }
   if (filter === "movieName") {
-  const sortedMovies = moviesData.sort((a, b) =>
-    a.Title.localeCompare(b.Title)
-  );
+  const sortedMovies = moviesData.sort((a, b) =>a.Title.localeCompare(b.Title));
 
-  let movies = document.querySelector(".movie__list");
+  let movies = document.querySelector(".movie__lists");
   movies.innerHTML = sortedMovies.map(movie => movieTime(movie)).join("");
 }
 }
@@ -26,7 +24,7 @@ movies.innerHTML=filteredMovies.map(movie=>movieTime(movie)).join("")
 showTime()
  function movieTime(movie){
     return `
-    <div class="movie__lists">
+   
     <div class="movie__list">
     <figure class="movieImage">
         <img src="${movie.Poster}" alt="${movie.Title}" class="movielogo">
@@ -35,13 +33,13 @@ showTime()
     <h4>${movie.Type}</h4>
     <h4>${movie.Year}</h4>
     <h4>${movie.imdbID}</h4>
- </div>
+ 
  </div>`
  }
  function movieSearch(event){
     console.log(event)
     showTime(event.target.value)//The value event.target.value is same as filter==="movieName" in if condition 
-  
+  //here we are calling showTime()function with value what we get onchanging the filter and based on value we choose then comes the if condtion on value we got after changing
  }
  
  
